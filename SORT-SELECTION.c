5b9b187b4c0390af24e807ba93d0553f4fba7855
HARSHTEOTIA23 <114357302+HARSHTEOTIA23@users.noreply.github.com>
2022-11-03 14:27:56 +0530

    Add files via upload

---
+++ SORT-SELECTION.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{ int a[100],n,i,j,swap,position;
+  printf("enter the no.of element\n");
+  scanf("%d",&n);
+  printf("enter the elements\n");
+  for(i=0;i<n;i++)
+    {  scanf("%d",&a[i]);}
+  for(i=0;i<n-1;i++)
+    {position=i;
+    for(j=i+1;j<n;j++)
+      {if(a[position]>a[j])
+      position=j;}
+  if(position!=i)
+  { swap=a[i];
+    a[i]=a[position];
+  a[position]=swap;
+      }}
+  printf("the required sorted array is:\n");
+  for(i=0;i<n;i++)
+    {printf("%d\n",a[i]);}
+return 0;
+}
