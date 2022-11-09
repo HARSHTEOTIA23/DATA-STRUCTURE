6cc20ca26cbc0cb8286c9f2c1016cf79907a31a5
HARSHTEOTIA23 <114357302+HARSHTEOTIA23@users.noreply.github.com>
2022-11-09 23:45:26 +0530

    file added

---
+++ matrix-subtraction.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{ int a[10][10],b[10][10],c[10][10],x,y,j,i;
+  printf("enter the size of matrix\n");
+  scanf("%d %d",&x,&y);
+  printf("enter the matrix 1\n");
+  
+  for(i=1;i<=x;i++)
+    for(j=1;j<=y;j++)
+    {
+        scanf("%d",&a[i][j]);
+    }
+  for(i=1;i<=x;i++)
+    { for(j=1;j<=y;j++)
+    {
+        printf("%d ",a[i][j]);
+    }
+      printf("\n");
+      }
+  
+  printf("enter the  matrix 2\n");
+  for(i=1;i<=x;i++)
+    for(j=1;j<=y;j++)
+    {
+        scanf("%d",&b[i][j]);
+    }
+  for(i=1;i<=x;i++)
+    { for(j=1;j<=y;j++)
+    {
+        printf("%d ",b[i][j]);
+    }
+      printf("\n");
+      }
+  
+for(i=1;i<=x;i++)
+    for(j=1;j<=y;j++)
+    {
+        c[i][j]=a[i][j]-b[i][j];
+      
+    }
+printf("the difference is is \n");
+for(i=1;i<=x;i++)
+    { for(j=1;j<=y;j++)
+    {
+        printf("%d ",c[i][j]);
+    }
+    printf("\n");
+      }
+  return 0;
+}
